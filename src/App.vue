f2b7bb15c444f9faeb6241bd39ee924178d2f963
Pedro Bernardes <phec06@gmail.com>
2016-12-24 18:41:43 -0200

    Sass >>>>>>> CSS

--- src/App.vue
+++ src/App.vue
@@ -14,13 +14,13 @@ exports.default =
   // Look! Another tag that will be replaced with a component
 </template>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="sass">
+#app
+  border: thin gray solid
+  font-family: 'Avenir', Helvetica, Arial, sans-serif
+  -webkit-font-smoothing: antialiased
+  -moz-osx-font-smoothing: grayscale
+  text-align: center
+  color: #2c3e50
+  margin-top: 60px
 </style>
